53b0e3e9f719bb0e86c0978996227f3392d3cb13
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 14:46:56 +0530

    [sonamahapatra] Added Welcome Message

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Logical Program");
+        }
+    }
+}
