f0ba7e8a141e758fb73e6eb5c7800de88b717691
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 15:02:01 +0530

    [sonamahapatra] Checking Prime Number

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public static void CheckPrime()
+        {
+            int n, i, m = 0, num = 0;
+            Console.WriteLine("Enter The Number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Number is not Prime");
+                    num = 1;
+                    break;
+                }
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,6 +11,20 @@ namespace LogicalPrograms
          public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Logical Program");
-        }
+            Console.WriteLine("Please Chhose One From Below Option: ");
+            Console.WriteLine("1.PrimeNumber\n");
+            int Option = Convert.ToInt32(Console.ReadLine());   
+            switch(Option)
+            {
+                case 1:
+                    PrimeNumber.CheckPrime();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose program with given Option");
+                    break;
+
+            }
+            Console.ReadLine();
+}
     }
 }
