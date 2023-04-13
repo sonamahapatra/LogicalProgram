14a8b2cb8f992382bfbf7b72ecf033b0417077d4
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 15:07:35 +0530

    [sonamahapatra] Reverse a number

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -12,12 +12,15 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Welcome To The Logical Program");
             Console.WriteLine("Please Chhose One From Below Option: ");
-            Console.WriteLine("1.PrimeNumber\n");
+            Console.WriteLine("1.PrimeNumber\n 2.Reverse Number");
             int Option = Convert.ToInt32(Console.ReadLine());   
             switch(Option)
             {
                 case 1:
                     PrimeNumber.CheckPrime();
+                    break;
+                    case 2:
+                    ReverseNumber.Display();
                     break;
                 default:
                     Console.WriteLine("Please Choose program with given Option");
